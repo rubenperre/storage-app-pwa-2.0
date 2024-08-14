c66cb1af58c9d82739292d5a9f9f0dea1c7d2a60
rubenperre <ruben.perre@hotmail.com>
2024-08-14 12:18:06 +0200

    add prettier + reformat code

---
+++ README.md
@@ -0,0 +1,3 @@
+# storage-closet-pwa
+
+React PWA (Hybrid app) for managing food &amp; drinks storage
---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,5 @@
+import {precacheAndRoute} from "workbox-precaching/precacheAndRoute";
+
+precacheAndRoute([
+    {revision: "8af3ef701c6f26517d794fd5719e4108", url: "index.html"},
+]);
